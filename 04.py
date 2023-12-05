d713203caf4808bf380fb802d3b79f6a4b6641fc
Rajiv Giridharagopal <rgiri@uw.edu>
2023-12-05 00:52:35 -0800

    Add files via upload

--- 04.py
+++ 04.py
@@ -7,23 +7,6 @@ data = [x.strip() for x in file.readlines()]
 import re
 import numpy as np
 
-# Part 1
-points = 0
-for d in data:
-    _d = [x.strip() for x in d.split(':')[1:]]
-    _d[0] = _d[0] + ' ' # for regex searching below
-    winners, numbers = _d[0].split('|')
-    winners = [int(x) for x in re.findall('\d+\s', winners)]
-    numbers = [int(x) for x in re.findall('\d+\s', numbers)]
-
-    # Use set intersection to find the points
-    matches = set(winners).intersection(set(numbers))
-    if any(matches):
-        points += 2** (len(matches)-1)
-
-print('Part 1:', points)
-
-# Part 2
 def evalgame(game):
     _d = [x.strip() for x in game.split(':')[1:]]
     _d[0] = _d[0] + ' ' # for regex searching below
@@ -36,12 +19,21 @@ def evalgame(game):
     
     return len(matches)
 
+# Part 1
+points = 0
+for d in data:
+    matches = evalgame(d)
+    points += 2** (max(0, matches-1)) #max avoids the case of 0 matches
+
+print('Part 1:', points)
+
+# Part 2
 games = np.ones(len(data), dtype=int) # Each game evaluated once
 
 # Count matches; the next N games count the current game number
 for n, d in enumerate(data):
         
     matches = evalgame(d)
-    games[np.arange(n+1,n+1+matches)] += games[n]
+    games[np.arange(n+1, n+1+matches)] += games[n]
 
 print('Part 2:', sum(games))
